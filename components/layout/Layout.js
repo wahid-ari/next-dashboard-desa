3231ee82a7eb6e20caecea6fc483ed873575f24f
wahid-ari <wahiid.arii@Gmail.com>
2022-11-06 00:52:09 +0700

    update

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Breadcrumb from "@components/systems/Breadcrumb";
 import Navbar from "./Navbar";
 import Menu from './Menu'
+import clsx from 'clsx';
 
 export default function Layout({ children, title, description }) {
   // Fix Warning: A title element received an array with more than 1 element as children.In browsers title Elements can only have Text Nodes as ldren.If the children being rendered output more than a single text node in aggregate the browser will display markup and comments as text in
@@ -23,17 +24,20 @@ export default function Layout({ children, title, description }) {
 
         <Sidebar />
 
-        <div>
+        <div className="relative">
 
           <Navbar />
 
           {/* Show on Mobile */}
-          <div className="sm:hidden flex gap-x-4 items-center justify-between border-b dark:border-neutral-800 pl-1 pr-3 sm:px-2 py-3 sticky top-0 bg-[#F8F9FD]/95 dark:bg-neutral-900/90 supports-[backdrop-filter]:backdrop-blur-sm z-[49] overflow-x-auto scrollbar scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-gray-300 dark:scrollbar-thumb-neutral-700">
+          <div className={clsx("xl:hidden flex gap-x-4 items-center justify-between border-b dark:border-neutral-800 pl-1 pr-3 sm:px-2 py-3",
+            "bg-[#F8F9FD]/95 dark:bg-neutral-900/90 overflow-x-auto",
+            "scrollbar scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-gray-300 dark:scrollbar-thumb-neutral-700")}>
             <Breadcrumb />
           </div>
 
           {/* Show on Desktop */}
-          <div className="hidden sm:flex gap-x-4 items-center justify-between border-b dark:border-neutral-800 pl-1 pr-3 sm:px-2 py-3 sticky top-0 bg-[#F8F9FD]/95 dark:bg-neutral-900/90 supports-[backdrop-filter]:backdrop-blur-sm z-[49]">
+          <div className={clsx("hidden xl:flex gap-x-4 items-center justify-between border-b dark:border-neutral-800 pl-1 pr-3 sm:px-2 py-3",
+            "z-40 sticky top-0 bg-[#F8F9FD]/95 dark:bg-neutral-900/90 supports-[backdrop-filter]:backdrop-blur-sm")}>
             <Breadcrumb />
             <Menu />
           </div>
--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -13,14 +13,14 @@ export default function Navbar() {
   };
 
   return (
-    <div className="xl:hidden h-11 bg-white dark:bg-neutral-900 dark:text-neutral-50 w-full flex justify-between items-center gap-4 pl-2 pr-3 sm:pl-3 sm:pr-4 border-b dark:border-neutral-800">
+    <div className="xl:hidden sticky top-0 z-40 h-11 bg-white dark:bg-neutral-900 dark:text-neutral-50 w-full flex justify-between items-center gap-4 pl-2 pr-3 sm:pl-3 sm:pr-4 border-b dark:border-neutral-800">
       <div className="flex gap-x-4">
         <button className="outline-none" id="menu" aria-label="Menu">
           <MenuIcon className="h-5 w-5" onClick={showMenu} />
         </button>
         <p className="text-sm text-center font-bold xl:text-2xl text-neutral-800 dark:text-neutral-100">Desa Digital</p>
       </div>
-      
+
       <div className="flex items-center gap-3">
 
         <div className="cursor-pointer pt-1">
@@ -35,7 +35,7 @@ export default function Navbar() {
           }
         </div>
 
-        <Menu className="sm:hidden" />
+        <Menu className="xl:hidden" />
 
       </div>
     </div>
